50dc3920f645d38ac44d30c8ea8283a58e78651c
Evan <fralinev@gmail.com>
2025-01-29 14:15:07 -0700

    modify HTML plugin

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,15 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './client/public/index.html',
+      inject: 'body',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+      },
+      // Prevent HTML escaping of the script tag:
+      scriptLoading: 'blocking',
+      xhtml: true,
     }),
   ],
   devServer: {
