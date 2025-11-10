1d9d12c9559296e0188f98061b71ab0a57f7baae
Evan <fralinev@gmail.com>
2025-11-10 10:30:02 -0500

    move git file

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', marginTop: "100px"}}> HELLO
+    </div>
+  );
+}
