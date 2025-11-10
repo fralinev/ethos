521acda3d69f9598034e0feea794187da47ab488
Evan <fralinev@gmail.com>
2025-11-10 10:33:11 -0500

    test commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', marginTop: "100px"}}> HELLO
+    <div style={{display: 'flex', justifyContent: 'center', marginTop: "100px"}}> HELLO WORLD
     </div>
   );
 }
