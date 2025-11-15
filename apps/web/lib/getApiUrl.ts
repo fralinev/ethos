647c6f113e8c8babc84c4bbde08db19ce3997c58
Evan <fralinev@gmail.com>
2025-11-15 13:50:44 -0500

    checkk

--- apps/web/lib/getApiUrl.ts
+++ apps/web/lib/getApiUrl.ts
@@ -1,5 +1,6 @@
 export function getApiUrl() {
   const host = process.env.NEXT_PUBLIC_API_HOST; // e.g. "ethos-api-vbj7"
+  console.log("checkk host", host)
   if (process.env.NODE_ENV === "production") {
     return `https://${host}.onrender.com`;
   }
