f1eba5a30e0565482f6c7a63c564a82c5b835c1f
Evan <fralinev@gmail.com>
2025-11-16 09:28:35 -0500

    fix cookie sameSite attribute

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -38,7 +38,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production" || false,
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60, // 1 hour
     },
   })
